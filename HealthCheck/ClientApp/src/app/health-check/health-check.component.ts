94b29d1737e8edfb2f9a0cd3d7ae7fbabca5846d
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-06-04 16:53:30 -0300

    Configuração adicionada

--- HealthCheck/ClientApp/src/app/health-check/health-check.component.ts
+++ HealthCheck/ClientApp/src/app/health-check/health-check.component.ts
@@ -1,1 +1,38 @@
-﻿
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-health-check',
+  template: './health-check.component.html',
+  styleUrls: ['./health-check.component.css']
+})
+
+export class HealthCheckComponent implements OnInit {
+  public result: Result;
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) {
+  }
+
+  ngOnInit() {
+    this.http.get<Result>(this.baseUrl + 'hc').subscribe(result => {
+      this.result = result;
+    }, error => console.error(error))
+  }
+}
+
+interface Result {
+  checks: Check[];
+  totalStatus: string;
+  totalResponseTime: number;
+
+}
+
+interface Check {
+  name: string;
+  status: string;
+  responseTime: number;
+
+}
+

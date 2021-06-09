e05b7b1f2edae3147cae1eb4c347982ec720b12b
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-06-09 17:32:05 -0300

    Adicionando arquivos .html, .css, .ts para o componente TestForm

---
+++ HealthCheck/ClientApp/src/app/test-form/test-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-form',
+  templateUrl: './test-form.component.html',
+  styleUrls: ['./test-form.component.css']
+
+})
+
+
+export class TestFormComponent {}
+
+
+
+
+

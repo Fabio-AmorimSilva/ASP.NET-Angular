3a343160261ed2f7c30983d739cfacaa3a006ad9
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-06-10 16:17:36 -0300

    Adicionando TestTableComponent para uso

--- HealthCheck/ClientApp/src/app/app-routing.module.ts
+++ HealthCheck/ClientApp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
 import { TestFormComponent } from './test-form/test-form.component';
+import { TestTableComponent } from './test-table/test-table.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'health-check', component: HealthCheckComponent },
-  { path: 'test-form', component: TestFormComponent }
+  { path: 'test-form', component: TestFormComponent },
+  { path: 'test-table', component: TestTableComponent }
 ];
 
 @NgModule({

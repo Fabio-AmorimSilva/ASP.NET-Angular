cbf10c0d4fac7cf3e9e28a815fa1b6f1b2eb3aee
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-06-12 17:50:36 -0300

    Adicionando component TestImage

--- HealthCheck/ClientApp/src/app/app-routing.module.ts
+++ HealthCheck/ClientApp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
 import { TestFormComponent } from './test-form/test-form.component';
 import { TestTableComponent } from './test-table/test-table.component';
+import { TestImageComponent } from './test-image/test-image.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'health-check', component: HealthCheckComponent },
   { path: 'test-form', component: TestFormComponent },
-  { path: 'test-table', component: TestTableComponent }
+  { path: 'test-table', component: TestTableComponent },
+  { path: 'test-image', component: TestImageComponent }
 ];
 
 @NgModule({

17e208d94a05c64afcfbb10d463eef41eda07746
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-06-23 16:58:36 -0300

    Adição de chave estrangeira para EF Core

--- WorldCities/WorldCities/Data/Models/City.cs
+++ WorldCities/WorldCities/Data/Models/City.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WorldCities.Data.Models
 {
+    [Table("Cities")]
     public class City
     {
 
@@ -43,13 +45,20 @@ namespace WorldCities.Data.Models
         /// City Longitude
         /// </summary>
         public decimal Lon { get; set; }
-        #endregion
-
+        
         ///<summary>
         /// Country Id (foreign key)
         /// </summary>
+        [ForeignKey(nameof(Country))]
         public int CountryId { get; set; }
+        #endregion
 
+        #region Navigation Properties
+        ///<summary>
+        /// The country related to this city
+        /// </summary>
+        public virtual Country Country { get; set; }
+        #endregion
 
     }
 }

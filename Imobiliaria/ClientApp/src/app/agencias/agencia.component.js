1391138c90baca7d05eaaf231fbbef190d85203a
unknown <fabioamorim2025@hotmail.com>
2021-09-02 18:08:31 -0300

    Arquivos de configuração para o componente agências

---
+++ Imobiliaria/ClientApp/src/app/agencias/agencia.component.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AgenciaComponent = void 0;
+var AgenciaComponent = /** @class */ (function () {
+    function AgenciaComponent() {
+    }
+    return AgenciaComponent;
+}());
+exports.AgenciaComponent = AgenciaComponent;
+//# sourceMappingURL=agencia.component.js.map
---
+++ Imobiliaria/ClientApp/src/app/agencias/agencia.ts
@@ -0,0 +1,7 @@
+export interface Agencia {
+  id: number;
+  nome: string;
+  cidade: string;
+  idCorretores: number;
+  idImoveis: number;
+}

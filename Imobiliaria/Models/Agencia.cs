6b3c50ce9d0502f38dbe36b8082a3cfdc23f1738
unknown <fabioamorim2025@hotmail.com>
2021-09-03 17:14:49 -0300

    Adicionando modelo da entidade Agência

---
+++ Imobiliaria/Models/Agencia.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imobiliaria.Models
+{
+    [Table("Agencias")]
+    public class Agencia
+    {
+        #region Constructor
+        public Agencia()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        ///<summary>
+        /// A chave única id e a chave primária
+        ///</summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        ///<summary>
+        /// Nome da agência (em formato UTF8)
+        ///</summary>
+        public string Nome { get; set; }
+
+        ///<summary>
+        /// Cidade onde a agência está localizada ( em formato UTF8)
+        ///</summary>
+        public string Cidade { get; set; }
+
+        ///<summary>
+        /// Id do corretor da agência
+        /// </summary>
+        public int idCorretor { get; set; }
+
+        ///<summary>
+        /// Id do imovel da agência
+        ///</summary>
+        public int idImovel { get; set; }
+
+        #endregion
+
+    }
+}

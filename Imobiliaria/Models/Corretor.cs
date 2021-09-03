ef249cd0424ead78e30c17ab642fc7e0ed799537
unknown <fabioamorim2025@hotmail.com>
2021-09-03 17:15:03 -0300

    Adicionando modelo da entidade Corretor

---
+++ Imobiliaria/Models/Corretor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imobiliaria.Models
+{
+    [Table("Corretores")]
+    public class Corretor
+    {
+        #region Constructor
+        public Corretor()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        ///<summary>
+        /// A chave única id e a chave primária
+        ///</summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        ///<summary>
+        /// Id da agência a qual o corretor pertence
+        ///</summary>
+        public int IdAgencia { get; set; }
+
+        ///<summary>
+        /// Nome do corretor
+        ///</summary>
+        public string Nome { get; set; }
+
+        ///<summary>
+        /// Idade do corretor
+        ///</summary>
+        public int Idade { get; set; }
+
+        ///<summary>
+        /// Número de vendas do corretor
+        ///</summary>
+        public int Vendas { get; set; }
+
+        #endregion
+    }
+}

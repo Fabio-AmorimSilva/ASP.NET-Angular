8de5fcce35884692ad4bdcab3411a49908ac6724
unknown <fabioamorim2025@hotmail.com>
2021-09-06 18:24:38 -0300

    Conexão com o banco de dados realizada

--- Imobiliaria/Startup.cs
+++ Imobiliaria/Startup.cs
@@ -27,19 +27,16 @@ namespace Imobiliaria
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /*
+           
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            */
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
 
             services.AddAuthentication()
                 .AddIdentityServerJwt();

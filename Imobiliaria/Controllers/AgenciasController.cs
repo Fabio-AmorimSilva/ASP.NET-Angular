5620b1f5b8e225b62cf6bfd3e1d50a1c2676153d
unknown <fabioamorim2025@hotmail.com>
2021-09-07 17:18:41 -0300

    Controlador de API para agências com operações de GET, POST, PUT e DELETE implementadas e funcionais

---
+++ Imobiliaria/Controllers/AgenciasController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Imobiliaria.Data;
+using Imobiliaria.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Imobiliaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgenciasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AgenciasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Agencias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Agencia>>> GetAgencias()
+        {
+            return await _context.Agencias.ToListAsync();
+        }
+
+        // GET: api/Agencias/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Agencia>> GetAgencia(int id)
+        {
+            var agencia = await _context.Agencias.FindAsync(id);
+
+            if (agencia == null)
+            {
+                return NotFound();
+            }
+
+            return agencia;
+        }
+
+        // PUT: api/Agencias/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAgencia(int id, Agencia agencia)
+        {
+            if (id != agencia.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(agencia).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgenciaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Agencias
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Agencia>> PostAgencia(Agencia agencia)
+        {
+            _context.Agencias.Add(agencia);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAgencia", new { id = agencia.Id }, agencia);
+        }
+
+        // DELETE: api/Agencias/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAgencia(int id)
+        {
+            var agencia = await _context.Agencias.FindAsync(id);
+            if (agencia == null)
+            {
+                return NotFound();
+            }
+
+            _context.Agencias.Remove(agencia);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AgenciaExists(int id)
+        {
+            return _context.Agencias.Any(e => e.Id == id);
+        }
+    }
+}

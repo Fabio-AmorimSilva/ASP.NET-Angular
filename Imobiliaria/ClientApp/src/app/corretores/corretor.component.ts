b8333c07494ced199529dfaf2d80daf60dc62d1c
unknown <fabioamorim2025@hotmail.com>
2021-09-08 16:56:49 -0300

    Arquivo .ts para componente corretores

---
+++ Imobiliaria/ClientApp/src/app/corretores/corretor.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Corretor } from './corretor';
+
+@Component({
+  selector: 'app-corretor',
+  templateUrl: './corretor.component.html',
+  styleUrls: ['./corretor.component.css']
+
+})
+
+export class CorretorComponent implements OnInit{
+  public corretores: Corretor[];
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) { }
+
+  ngOnInit() {
+    var url = this.baseUrl + 'api/Corretores';
+    this.http.get<Corretor[]>(url)
+      .subscribe(result => {
+        this.corretores = result;
+      }, error => console.error(error));
+  }
+
+
+
+}

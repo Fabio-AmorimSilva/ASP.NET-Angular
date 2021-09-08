32045767ece0f332474edf7ae5e91634d163bd0d
unknown <fabioamorim2025@hotmail.com>
2021-09-08 16:54:26 -0300

    Adicionando o CorretorComponent ao roteamento de componentes

--- Imobiliaria/ClientApp/src/app/app-routing.module.ts
+++ Imobiliaria/ClientApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciaComponent } from './agencias/agencia.component';
+import { CorretorComponent } from './corretores/corretor.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'agencias', component: AgenciaComponent }
+  { path: 'agencias', component: AgenciaComponent },
+  { path: 'corretores', component: CorretorComponent }
 
 ];
 

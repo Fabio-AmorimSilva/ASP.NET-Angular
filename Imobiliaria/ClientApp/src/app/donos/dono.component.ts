8a7527ba08be401d14c80677e81c3780d52aa379
unknown <fabioamorim2025@hotmail.com>
2021-09-09 16:22:47 -0300

    Adicionando arquivo .ts para donos

---
+++ Imobiliaria/ClientApp/src/app/donos/dono.component.ts
@@ -0,0 +1,29 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Dono } from './dono';
+
+@Component({
+  selector: 'app-dono',
+  templateUrl: 'dono.component.html',
+  styleUrls: ['./dono.component.css']
+
+})
+
+export class DonoComponent implements OnInit {
+  public donos: Dono[];
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) { }
+
+  ngOnInit() {
+    var url = this.baseUrl + 'api/Donos';
+    this.http.get<Dono[]>(url)
+      .subscribe(result => {
+        this.donos = result;
+      }, error => console.error(error));
+  }
+
+
+}

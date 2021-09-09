414e9a908b295f4b17bd341ea3762e01e0a94481
unknown <fabioamorim2025@hotmail.com>
2021-09-09 16:20:51 -0300

    Adicionando o DonoComponent ao roteamento de componentes

--- Imobiliaria/ClientApp/src/app/app-routing.module.ts
+++ Imobiliaria/ClientApp/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciaComponent } from './agencias/agencia.component';
 import { CorretorComponent } from './corretores/corretor.component';
+import { DonoComponent } from './donos/dono.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'agencias', component: AgenciaComponent },
-  { path: 'corretores', component: CorretorComponent }
+  { path: 'corretores', component: CorretorComponent },
+  { path: 'donos', component: DonoComponent }
 
 ];
 

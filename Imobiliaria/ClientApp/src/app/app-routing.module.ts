d87a84c1f87419fa7a15a04e4ff740d2834a5d62
unknown <fabioamorim2025@hotmail.com>
2021-09-12 15:55:24 -0300

    Adicionando ImovelComponent ao roteamento

--- Imobiliaria/ClientApp/src/app/app-routing.module.ts
+++ Imobiliaria/ClientApp/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { AgenciaComponent } from './agencias/agencia.component';
 import { CorretorComponent } from './corretores/corretor.component';
 import { DonoComponent } from './donos/dono.component';
+import { ImovelComponent } from './imoveis/imovel.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'agencias', component: AgenciaComponent },
   { path: 'corretores', component: CorretorComponent },
-  { path: 'donos', component: DonoComponent }
+  { path: 'donos', component: DonoComponent },
+  { path: 'imoveis', component: ImovelComponent }
 
 ];
 

aab7416797c9a0fb747ab47c3402a7c925898838
unknown <fabioamorim2025@hotmail.com>
2021-09-12 15:58:18 -0300

    Arquivo .ts para imovéis

---
+++ Imobiliaria/ClientApp/src/app/imoveis/imovel.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Imovel } from './imovel';
+
+@Component({
+  selector: 'app-imovel',
+  templateUrl: 'imovel.component.html',
+  styleUrls: ['./imovel.component.css']
+
+})
+
+export class ImovelComponent implements OnInit {
+  public imoveis: Imovel[];
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) { }
+
+  ngOnInit() {
+    var url = this.baseUrl + 'api/Imoveis';
+    this.http.get<Imovel[]>(url)
+      .subscribe(result => {
+        this.imoveis = result;
+      }, error => console.error(error));
+  }
+
+}

7bf46e459a4dec00504658c5ac9f430c92f96e13
unknown <fabioamorim2025@hotmail.com>
2021-09-15 19:37:34 -0300

    Adicionando arquivo .ts

---
+++ Imobiliaria/ClientApp/src/app/agencias/agencia-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Route } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-agencia-edit',
+  templateUrl: 'agencia-edit.component.html',
+  styleUrls: ['./agencia-edit.component.css']
+
+})
+
+export class AgenciaEditComponent implements OnInit {
+
+}

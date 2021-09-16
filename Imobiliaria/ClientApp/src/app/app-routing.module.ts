4091622711d27d84daee9d7e937001c447417ec1
unknown <fabioamorim2025@hotmail.com>
2021-09-16 19:14:30 -0300

    Adicionando AgenciaEdit

--- Imobiliaria/ClientApp/src/app/app-routing.module.ts
+++ Imobiliaria/ClientApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciaComponent } from './agencias/agencia.component';
+import { AgenciaEditComponent } from './agencias/agencia-edit.component';
 import { CorretorComponent } from './corretores/corretor.component';
 import { DonoComponent } from './donos/dono.component';
 import { ImovelComponent } from './imoveis/imovel.component';
@@ -10,6 +11,7 @@ import { ImovelComponent } from './imoveis/imovel.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'agencias', component: AgenciaComponent },
+  { path: 'agencia/:id', component: AgenciaEditComponent },
   { path: 'corretores', component: CorretorComponent },
   { path: 'donos', component: DonoComponent },
   { path: 'imoveis', component: ImovelComponent }

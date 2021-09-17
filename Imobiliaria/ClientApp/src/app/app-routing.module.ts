48b0760d026c92db27f26ff09e2622b25fcde808
unknown <fabioamorim2025@hotmail.com>
2021-09-17 17:29:39 -0300

    Adicionando CorretorEditComponent e seu caminho

--- Imobiliaria/ClientApp/src/app/app-routing.module.ts
+++ Imobiliaria/ClientApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AgenciaComponent } from './agencias/agencia.component';
 import { AgenciaEditComponent } from './agencias/agencia-edit.component';
 import { CorretorComponent } from './corretores/corretor.component';
+import { CorretorEditComponent } from './corretores/corretor-edit.component';
 import { DonoComponent } from './donos/dono.component';
 import { ImovelComponent } from './imoveis/imovel.component';
 
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'agencias', component: AgenciaComponent },
   { path: 'agencia/:id', component: AgenciaEditComponent },
   { path: 'corretores', component: CorretorComponent },
+  { path: 'corretor/:id', component: CorretorEditComponent },
   { path: 'donos', component: DonoComponent },
   { path: 'imoveis', component: ImovelComponent }
 

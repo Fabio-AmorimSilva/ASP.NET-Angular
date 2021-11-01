e2bbdfdd71fc9976513946100f18330fdcce297a
unknown <fabioamorim2025@hotmail.com>
2021-11-01 18:20:51 -0300

    Adicionando caminho para componente de remoção

--- Imobiliaria/ClientApp/src/app/app-routing.module.ts
+++ Imobiliaria/ClientApp/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciaComponent } from './agencias/agencia.component';
 import { AgenciaEditComponent } from './agencias/agencia-edit.component';
+import { AgenciaDeleteComponent } from './agencias/agencia-delete.component';
 import { CorretorComponent } from './corretores/corretor.component';
 import { CorretorEditComponent } from './corretores/corretor-edit.component';
 import { DonoComponent } from './donos/dono.component';
@@ -11,11 +12,13 @@ import { ImovelComponent } from './imoveis/imovel.component';
 import { ImovelEditComponent } from './imoveis/imovel-edit.component';
 
 
+
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'agencias', component: AgenciaComponent },
   { path: 'agencia/:id', component: AgenciaEditComponent },
   { path: 'agencia', component: AgenciaEditComponent },
+  { path: 'agencia-delete/:id', component: AgenciaDeleteComponent },
   { path: 'corretores', component: CorretorComponent },
   { path: 'corretor/:id', component: CorretorEditComponent },
   { path: 'corretor', component: CorretorEditComponent },

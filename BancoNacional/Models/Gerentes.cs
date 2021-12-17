5d99f961491c4558e7f0b16493f9ea7d72c1ae9c
unknown <fabioamorim2025@hotmail.com>
2021-12-17 16:53:07 -0300

    Adicionando o modelo para Gerentes

---
+++ BancoNacional/Models/Gerentes.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoNacional.Models
+{
+    [Table("GERENTES")]
+    public class Gerentes
+    {
+        public Gerentes() { }
+
+        [Key]
+        [Required]
+        ///<summary>
+        /// Chave primária da tabela GERENTES
+        /// </summary>
+        public int CODIGO_GERENTE { get; set; }
+
+        [ForeignKey("GERENTES")]
+        [Required]
+        public int CODIGO_AGENCIA { get; set; }
+
+        [Required]
+        public string NOME { get; set; }
+
+    }
+}

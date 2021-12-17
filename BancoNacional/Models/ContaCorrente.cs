de1635b9607880e8c98dbfbea6434b9a2a7a0696
unknown <fabioamorim2025@hotmail.com>
2021-12-17 16:53:39 -0300

    Adicionando o modelo para ContaCorrente

---
+++ BancoNacional/Models/ContaCorrente.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoNacional.Models
+{
+    [Table("CONTA_CORRENTE")]
+    public class ContaCorrente
+    {
+
+        public ContaCorrente() { }
+
+        /// <summary>
+        /// Chave primária da tabela CONTA_CORRENTE
+        /// </summary>
+        [Key]
+        [Required]
+        public int CODIGO_CONTA { get; set; }
+
+        [Required]
+        public string DONO { get; set; }
+
+
+    }
+}

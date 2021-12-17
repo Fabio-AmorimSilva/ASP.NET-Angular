e5b2cd469b34a8d7f6dfc542ea543b851dcf67a3
unknown <fabioamorim2025@hotmail.com>
2021-12-17 16:53:19 -0300

    Adicionando o modelo para Clientes

---
+++ BancoNacional/Models/Clientes.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoNacional.Models
+{
+    [Table("CLIENTES")]
+    public class Clientes
+    {
+        public Clientes(){}
+
+        ///<summary>
+        /// Chave primária da tabela CLIENTES
+        /// </summary>
+        [Key]
+        [Required]
+        public int CODIGO_CLIENTE { get; set; }
+
+        [ForeignKey("CODIGO_AGENCIA")]
+        [Required]
+        public int CODIGO_AGENCIA { get; set; }
+
+        [Required]
+        public int TIPO_CONTA { get; set; }
+
+        [Required]
+        public string NOME { get; set; }
+
+        [Required]
+        public int SALDO { get; set; }
+
+    }
+}

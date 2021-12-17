eb0b5e094084b4d9167c3cf5c51882c9305e7d87
unknown <fabioamorim2025@hotmail.com>
2021-12-17 16:47:34 -0300

    Adicionando o modelo para Agências

---
+++ BancoNacional/Models/Agencias.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoNacional.Models
+{
+
+    [Table("AGENCIAS")]
+    public class Agencias
+    {
+
+        public Agencias() { }
+
+        ///<summary>
+        /// Chave primária da tabelas AGENCIAS
+        /// </summary>
+        
+        [Key]
+        [Required]
+        public int CODIGO_AGENCIA { get; set; }
+
+        [Required]
+        public string NOME { get; set; }
+         
+        [Required]
+        public string LOCALIZACAO { get; set; }
+
+        [Required]
+        public int NUMERO_CLIENTES { get; set; }
+
+        [ForeignKey("GERENTE")]
+        [Required]
+        public int GERENTE { get; set; }
+
+
+    }
+
+}

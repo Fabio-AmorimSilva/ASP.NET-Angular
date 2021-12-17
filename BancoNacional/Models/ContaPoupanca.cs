d7117eb1aac10ee7ae16798cd9c670ea1dc24248
unknown <fabioamorim2025@hotmail.com>
2021-12-17 16:53:52 -0300

    Adicionando o modelo para ContaPoupanca

---
+++ BancoNacional/Models/ContaPoupanca.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoNacional.Models
+{
+    [Table("CONTA_POUPANCA")]
+    public class ContaPoupanca
+    {
+
+        public ContaPoupanca() { }
+
+        /// <summary>
+        /// Chave primária da tabela CONTA_POUPANCA
+        /// </summary>
+        [Key]
+        [Required]
+        public int CODIGO_CONTA { get; set; }
+
+        [Required]
+        public string DONO { get; set; }
+
+
+    }
+}

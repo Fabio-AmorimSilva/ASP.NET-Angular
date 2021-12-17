d191164f0092b30747642979bfceb6ec3d317af0
unknown <fabioamorim2025@hotmail.com>
2021-12-17 17:20:26 -0300

    Adicionando contexto do db com EF para ligação com sql server

---
+++ BancoNacional/Data/BancoNacionalDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BancoNacional.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancoNacional.Data
+{
+    public class BancoNacionalDbContext : DbContext
+    {
+        public BancoNacionalDbContext() : base(){}
+
+        public BancoNacionalDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Agencias> Agencias { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Gerentes> Gerentes { get; set; }
+        public DbSet<ContaCorrente> ContaCorrente { get; set; }
+        public DbSet<ContaPoupanca> contaPoupanca { get; set; }
+
+    }
+}

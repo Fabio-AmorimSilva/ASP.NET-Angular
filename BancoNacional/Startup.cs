f0822c6c9946d7159dd37b84b9f24f36892aa2b1
unknown <fabioamorim2025@hotmail.com>
2021-12-18 16:14:51 -0300

    Remoção de middlewares não utilizadas

--- BancoNacional/Startup.cs
+++ BancoNacional/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BancoNacional
 {
@@ -31,6 +32,8 @@ namespace BancoNacional
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +42,7 @@ namespace BancoNacional
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
             }
             else
             {
@@ -55,6 +59,7 @@ namespace BancoNacional
             }
 
             app.UseRouting();
+            app.UseCors();
 
             app.UseEndpoints(endpoints =>
             {

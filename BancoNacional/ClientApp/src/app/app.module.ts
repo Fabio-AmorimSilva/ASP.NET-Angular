7f34ff1bfd883a789c98b07b7674f3c65c372596
unknown <fabioamorim2025@hotmail.com>
2021-12-21 16:25:39 -0300

    Adicionando AgenciasComponent

--- BancoNacional/ClientApp/src/app/app.module.ts
+++ BancoNacional/ClientApp/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-
+import { AgenciasComponent } from './agencias/agencias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    AgenciasComponent
 
   ],
   imports: [

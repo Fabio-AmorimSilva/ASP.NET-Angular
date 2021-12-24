ca647c5d59798a514c8bd5f9060d4c0d8982ebe8
unknown <fabioamorim2025@hotmail.com>
2021-12-24 13:50:28 -0300

    Adicionando ClientesComponent ao roteamento

--- BancoNacional/ClientApp/src/app/app-routing.module.ts
+++ BancoNacional/ClientApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciasComponent } from './agencias/agencias.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'agencias', component: AgenciasComponent }
+  { path: 'agencias', component: AgenciasComponent },
+  { path: 'clientes', component: ClientesComponent }
 
 
 ];

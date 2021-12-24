091d36574a2a43550481010cce678a4e6aa9c22a
unknown <fabioamorim2025@hotmail.com>
2021-12-24 13:49:22 -0300

    Adicionando arquivo .ts para clientes com método GET implementado

---
+++ BancoNacional/ClientApp/src/app/clientes/clientes.component.ts
@@ -0,0 +1,36 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { cliente } from './cliente';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: 'clientes.component.html',
+  stylesUrl: ['./clientes.component.css']
+
+})
+
+export class ClientesComponent implements OnInit {
+
+  //Variável com os clientes
+  public clientes: cliente[];
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) { }
+
+  ngOnInit() {
+    this.loadClientes();
+
+  }
+
+  loadClientes() {
+    var url = this.baseUrl + 'api/Clientes';
+    this.http.get<cliente[]>(url)
+      .subscribe(result => {
+        this.clientes = result;
+
+      }, error => console.error(error));
+  }
+
+}

4130a7fb33337ad2642a5d11a18a94b7e9b9df13
unknown <fabioamorim2025@hotmail.com>
2021-12-26 14:11:11 -0300

    Adicionando GerentesComponent ao roteamento

--- BancoNacional/ClientApp/src/app/app-routing.module.ts
+++ BancoNacional/ClientApp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciasComponent } from './agencias/agencias.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { GerentesComponent } from './gerentes/gerentes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'agencias', component: AgenciasComponent },
-  { path: 'clientes', component: ClientesComponent }
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'gerentes', component: GerentesComponent }
 
 
 ];

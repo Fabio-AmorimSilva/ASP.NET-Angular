5036e2f2f95822ff5d007b11d5a6ed681575a109
unknown <fabioamorim2025@hotmail.com>
2022-01-02 17:19:10 -0300

    Adicionando AgenciaEditComponent ao roteamento

--- BancoNacional/ClientApp/src/app/app-routing.module.ts
+++ BancoNacional/ClientApp/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgenciasComponent } from './agencias/agencias.component';
+import { AgenciaEditComponent } from './agencias/agencia-edit.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { GerentesComponent } from './gerentes/gerentes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'agencias', component: AgenciasComponent },
+  { path: 'agencia', component: AgenciaEditComponent },
+  { path: 'agencia/:codigoAgencia', component: AgenciaEditComponent },
   { path: 'clientes', component: ClientesComponent },
   { path: 'gerentes', component: GerentesComponent }
 
